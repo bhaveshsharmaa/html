63b00786ae28d53a0d5989a961733e07b3cf7a2e
Bhavesh kumar sharma <bhaveshsharm.21@gmail.com>
2023-07-21 11:37:18 +0530

    added

---
+++ 100-50.c
@@ -0,0 +1,15 @@
+// 100-50
+#include<stdio.h>
+int main()
+{
+	int a=100;
+	while(a>=50)
+	{
+		printf("%d ",a);
+		a--;
+	}
+		return 0;
+	
+}
+
+
---
+++ 50-100.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() 
+{
+
+  
+	int a=50;
+	while(a<=100)
+	{
+		printf("%d ",a);
+		a++;
+	}
+   return 0;
+}
